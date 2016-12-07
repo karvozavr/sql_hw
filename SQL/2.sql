956138b6bbf06b12b7c3f8e673797542e943a543
Dmitriy Abramov (KARVOZAVR) <karvozavr@gmail.com>
2016-12-08 00:57:34 +0300

    WTF

--- SQL/2.sql
+++ SQL/2.sql
@@ -2,5 +2,5 @@ SELECT Country.Name, LiteracyRate.Rate FROM LiteracyRate
     INNER JOIN Country ON Country.Code = LiteracyRate.CountryCode
     GROUP BY Country.Name
     HAVING LiteracyRate.Year = MAX(LiteracyRate.Year)
-    ORDER BY Rate DESC
+    ORDER BY Rate DESC;
     LIMIT 1;
