cc033a886ffa121ff9bd763970ee58a18032a305
Dmitriy Abramov (KARVOZAVR) <karvozavr@gmail.com>
2016-12-07 10:20:47 +0300

    Done more

--- SQL/3.sql
+++ SQL/3.sql
@@ -1,4 +1,4 @@
 SELECT City.Name FROM Country
     JOIN City ON City.CountryCode = Country.Code
     JOIN Capital ON Capital.CityID = City.ID
-    WHERE Country.Name LIKE "%Malaysia%";
+    WHERE Country.Name LIKE "Malaysia";
